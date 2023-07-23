df7cd2e68e68b8ac47fbe8f79208d2ef95dafc4f
AkumaCtrl <55247600+AkumaCtrl@users.noreply.github.com>
2023-07-23 19:47:21 +0200

    Delete AkumaCtrl.github.io-main directory

--- AkumaCtrl.github.io-main/javascript.js
+++
@@ -1,6 +0,0 @@
-const navMenu = document.querySelector('.js-nav-menu');
-const toggleButton = document.querySelector('.js-nav-menu-toggle');
-
-toggleButton.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
